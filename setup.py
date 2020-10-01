737057fca6a467c7d41f2643e14464851f991ec3
Nicolas Tessore <nicolas.tessore@manchester.ac.uk>
2020-10-01 17:35:19 +0100

    create skypy-data package

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='skypy-data',
+    version='0.1.0',
+    author='SkyPy Project',
+    description='data files for the SkyPy library',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/skypyproject/data',
+    packages=[
+        'skypy-data.bandpasses',
+        'skypy-data.spectrum_templates',
+    ],
+    package_dir={
+        'skypy-data': '.'
+    },
+    package_data={
+        '': ['*.ecsv']
+    },
+)
